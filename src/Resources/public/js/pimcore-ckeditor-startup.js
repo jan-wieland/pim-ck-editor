2496e9cb14a5a592160dd4a0c9669b019be7cb3b
Jan Wieland <info@jan-wieland.de>
2026-05-25 17:44:41 +0200

    Fix JS initializeWysiwyg error by registering provider

--- src/Resources/public/js/pimcore-ckeditor-startup.js
+++ src/Resources/public/js/pimcore-ckeditor-startup.js
@@ -1,7 +1,34 @@
-(function () {
-  if (!parent.pimcore.wysiwyg) {
-    parent.pimcore.wysiwyg = {};
-    parent.pimcore.wysiwyg.editors = [];
-  }
-  parent.pimcore.wysiwyg.editors.push("CKEditor5");
-})();
+pimcore.registerNS("pimcore.plugin.pimckeditor");
+
+pimcore.plugin.pimckeditor = Ext.extend(pimcore.plugin.admin, {
+  getClassName: function () {
+    return "pimcore.plugin.pimckeditor";
+  },
+
+  initialize: function () {
+    pimcore.plugin.broker.registerPlugin(this);
+
+    // Das ist der entscheidende Fix für Pimcore 11:
+    // Wir registrieren den CKEditor als WYSIWYG-Provider, damit
+    // Pimcore weiß, wo es 'initializeWysiwyg' aufrufen kann!
+    if (pimcore.wysiwyg) {
+      pimcore.wysiwyg.registerProvider("ckeditor", {
+        initializeWysiwyg: function (id, config) {
+          // Hier wird die CKEditor5-Instanz auf dem Textarea-Feld gestartet
+          if (typeof ClassicEditor !== "undefined") {
+            ClassicEditor.create(document.getElementById(id), config).catch(
+              (error) => {
+                console.error(error);
+              },
+            );
+          }
+        },
+      });
+
+      // Setze CKEditor als Standard, falls Quill aus ist
+      pimcore.wysiwyg.defaultProvider = "ckeditor";
+    }
+  },
+});
+
+new pimcore.plugin.pimckeditor();
