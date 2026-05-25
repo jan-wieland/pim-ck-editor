36f4a86d3b62e6fa5872eb44866ec2b23b7a0a17
Jan Wieland <info@jan-wieland.de>
2026-05-25 15:55:56 +0200

    fixed: use as global editor

--- src/PimCkEditorBundle.php
+++ src/PimCkEditorBundle.php
@@ -1,41 +1,38 @@
 <?php
-
 namespace JanWieland\PimCkEditorBundle;
 
 use Pimcore\Extension\Bundle\AbstractPimcoreBundle;
+use Pimcore\Extension\Bundle\PimcoreBundleAdminClassicInterface;
+use Pimcore\Extension\Bundle\Traits\BundleAdminClassicTrait;
 
-class PimCkEditorBundle extends AbstractPimcoreBundle
+class PimCkEditorBundle extends AbstractPimcoreBundle implements
+    PimcoreBundleAdminClassicInterface
 {
+    use BundleAdminClassicTrait;
+
+    public function getPath(): string
+    {
+        return dirname(__DIR__);
+    }
+
     public function getJsPaths(): array
     {
-        return [
-            '/bundles/pimckeditor/js/pimcore-ckeditor-startup.js',
-        ];
+        return ["/bundles/pimckeditor/js/pimcore-ckeditor-startup.js"];
     }
 
     public function getEditmodeJsPaths(): array
     {
         return [
-            '/bundles/pimckeditor/js/ckeditor5.umd.js',
-            '/bundles/pimckeditor/js/pimcore-ckeditor.js',
+            "/bundles/pimckeditor/js/ckeditor5.umd.js",
+            "/bundles/pimckeditor/js/pimcore-ckeditor.js",
         ];
     }
 
     public function getEditmodeCssPaths(): array
     {
         return [
-            '/bundles/pimckeditor/css/ckeditor5.css',
-            '/bundles/pimckeditor/css/pimcore-ckeditor.css',
+            "/bundles/pimckeditor/css/ckeditor5.css",
+            "/bundles/pimckeditor/css/pimcore-ckeditor.css",
         ];
-    }
-
-    public function getNiceName(): string
-    {
-        return 'CKEditor 5 for Pimcore';
-    }
-
-    public function getDescription(): string
-    {
-        return 'Replaces Quill with CKEditor 5 including full link dialog with target, CSS classes and Pimcore page browser.';
     }
 }
