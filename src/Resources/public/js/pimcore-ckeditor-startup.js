b2ebe1b146426a61934daebe52185f6ca06617a2
Jan Wieland <info@jan-wieland.de>
2026-05-25 03:45:18 +0200

    register in PIM

---
+++ src/Resources/public/js/pimcore-ckeditor-startup.js
@@ -0,0 +1,7 @@
+(function () {
+  if (!parent.pimcore.wysiwyg) {
+    parent.pimcore.wysiwyg = {};
+    parent.pimcore.wysiwyg.editors = [];
+  }
+  parent.pimcore.wysiwyg.editors.push("CKEditor5");
+})();
